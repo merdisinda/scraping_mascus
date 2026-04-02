300b8b616c0343b5a3fdf5cbb16b2296bea3728a
rokheyambaye <rokheya.mbaye.bc@gmail.com>
2026-04-02 12:40:07 +0200

    code a modifier

--- scrapping.py
+++ scrapping.py
@@ -4,9 +4,10 @@ import time
 import random
 import pandas as pd
 
-# FONCTION PAUSE
-def human_sleep(min_sec=2, max_sec=5):
+
+def sleep(min_sec=2, max_sec=5):
     time.sleep(random.uniform(min_sec, max_sec))
+
 
 
 # CONFIG
@@ -14,100 +15,113 @@ NB_PAGES = 358
 BASE_URL = "https://www.mascus.com/transportation/trucks?page={}"
 
 driver = webdriver.Chrome()
-
 all_links = []
 
 
 # ETAPE 1 : LIENS
-#print("🔎 Récupération des liens...")
+#print("Récupération des liens...")
+
+
 
 for page in range(1, NB_PAGES + 1):
-    #print(f"Page {page}/{NB_PAGES}")
-    
     driver.get(BASE_URL.format(page))
-    human_sleep(4, 7)  #  pause plus longue pour chargement
-
+    sleep(4, 5)  #  pause plus longue pour chargement
     trucks = driver.find_elements(By.CSS_SELECTOR, "div[class*='SearchResult_searchResultItemWrapper']")
 
+    
     for truck in trucks:
         try:
             link = truck.find_element(By.CSS_SELECTOR, "a[href*='/transportation/']").get_attribute("href")
             if link:
                 all_links.append(link)
         except:
-            pass
+                 pass
 
-    # petite pause entre pages
-    human_sleep(2, 4)
+# petite pause entre pages
 
+    sleep(2, 4)
     # sauvegarde
+
     if page % 20 == 0:
-        pd.DataFrame(all_links, columns=["link"]).to_csv("backup_links.csv", index=False)
+        pd.DataFrame(all_links, columns=["link"]).to_csv("liens_recuperes.csv", index=False)
+
+
 
 # enlever doublons
 all_links = list(set(all_links))
-
 print(f"i{len(all_links)} liens récupérés")
 
 
+
 # ETAPE 2 : DETAILS
+
 data = []
-
 for i, link in enumerate(all_links):
-    #print(f"{i+1}/{len(all_links)}")
-
     driver.get(link)
-    human_sleep(3, 6)
-
+    sleep(4,6)
     try:
         info = {}
         items = driver.find_elements(By.CSS_SELECTOR, "div.key-value-wrapper")
-
         for item in items:
             try:
+
                 label = item.find_element(By.CLASS_NAME, "key-value-label").text.strip()
                 value = item.find_element(By.CLASS_NAME, "key-value-value").text.strip()
                 info[label] = value
+
             except:
+
                 pass
 
         # titre
+
         try:
             title = driver.find_element(By.TAG_NAME, "h1").text
+
         except:
             title = None
 
+        
         # prix
-        try:
-            price = driver.find_element(By.CSS_SELECTOR, "div.heading5").text
-        except:
-            price = None
+        price = None
+        items = driver.find_elements(By.CSS_SELECTOR, "div.key-value-wrapper")
+        for item in items:
+            try:
+                label = item.find_element(By.CLASS_NAME, "key-value-label").text.strip()
+        
+                if "Price Including Tax" in label:
+                    price = item.find_element(By.CLASS_NAME, "key-value-value").text.strip()
+            except:
+                 pass
+
 
         data.append({
+
             "titre": title,
             "prix": price,
             "annee": info.get("Year"),
-            "km": info.get("Meter read-out"),
+            "Km": info.get("Meter read-out"),
             "pays": info.get("Country"),
-            "type": info.get("Category"),
+             "type": info.get("Category"),
             "modele": info.get("Brand / model"),
-            "lien": link
+             "lien": link
+
         })
 
     except Exception as e:
         print("Erreur :", e)
 
-    # pause entre chaque camion
-    human_sleep(2, 5)
-
-    # sauvegarde
-    if i % 50 == 0:
-        pd.DataFrame(data).to_csv("backup_links.csv", index=False)
+# pause entre chaque camion
+sleep(3, 5)
+# sauvegarde
+if i % 50 == 0:
+    pd.DataFrame(data).to_csv("mascus_camions.csv", index=False)
 
 driver.quit()
+
+
 
 # EXPORT FINAL
 df = pd.DataFrame(data)
 df.to_csv("camions_mascus.csv", index=False)
-
-print(" Scraping terminé ")
+print(" Scrapping terminé ") 
